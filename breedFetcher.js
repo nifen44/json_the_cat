c49b4402923454b33a17915d97ca79ff70b484de
Wei Huang <huangwei19810803@gmail.com>
2023-05-11 12:45:13 -0600

    using module and mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,12 @@
 const request = require('request');
 
-const searchCat = (name) =>{
-    request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (error, response, body) =>{
-        // console.log('statusCode: ', response && response.statusCode);
-        if(error){
-            console.log(error);
-        }else{
-            const data = JSON.parse(body);
-            console.log(data.length !== 0 && data[0].description);
-        }       
-})
-}
+const fetchBreedDescription = (breedName, callback) =>{
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) =>{
+    const data = JSON.parse(body);
+    const desc = (data.length !== 0 ? data[0].description : null);
+    callback(error, desc);
+  });
+};
 
+module.exports = { fetchBreedDescription };
 
-const args = process.argv;
-console.log(searchCat(args.slice(2)));
